e61b665499df4edb0530c7c7c14c55e8ecf1cd72
sawada-t <takumi.sawada@mosh-bit.com>
2020-12-18 19:21:54 +0900

    commit

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ install_requires = [
 
 packages = [
     'privutils',
+    'privutils.api_util',
+    'privutils.batch_util',
+    'privutils.mysql_util',
+    'privutils.self_scheduling_job'
 ]
 
 console_scripts = [
@@ -15,7 +19,7 @@ console_scripts = [
 
 setup(
     name='privutils',
-    version='0.0.6',
+    version='0.0.7',
     packages=packages,
     install_requires=install_requires,
 )
